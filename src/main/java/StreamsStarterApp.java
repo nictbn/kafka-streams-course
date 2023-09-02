c4f90f5f82364f44157afe2c16124c8b203eb69b
Nicu Taban <nictbndeveloper@gmail.com>
2023-09-02 18:14:03 +0300

    Word Count Topology Configuration

--- src/main/java/StreamsStarterApp.java
+++ src/main/java/StreamsStarterApp.java
@@ -1,7 +1,14 @@
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Produced;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class StreamsStarterApp {
@@ -12,5 +19,18 @@ public class StreamsStarterApp {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+        KTable<String, Long> wordCounts = wordCountInput
+                .mapValues(value -> value.toLowerCase())
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .selectKey((ignored, word) -> word)
+                .groupByKey()
+                .count(Named.as("Counts"));
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+        try (KafkaStreams streams = new KafkaStreams(builder.build(), config)) {
+            streams.start();
+        }
     }
 }
